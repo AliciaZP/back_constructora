a29c763760b3f42f1a1b24f23b26077fb1fff483
Luis Alberto Ferrando Sanchez <luisalbertoferrando@gmail.com>
2023-12-05 13:33:45 +0100

    Se ponen cosas de configuracion basica que faltan

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.use('/admins', require('./api/admin'));
+router.use('/admins', require('./api/admins'));
 router.use('/workers', require('./api/workers'));
 router.use('/constructions', require('./api/constructions'));
 
