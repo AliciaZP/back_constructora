715423cba2a50c3221635162be94178bfe97abd8
Guillermo Vidal <Guillle912@gmail.com>
2023-12-05 11:03:57 +0100

    funcion CreateToken

---
+++ src/helpers/utils.js
@@ -0,0 +1,16 @@
+const dayjs = require('dayjs');
+const jwt = require('jsonwebtoken');
+
+const createToken = ( user ) => {
+    const payload = {
+        user_id: user.id,
+        user_role: user.role,
+    }
+
+    return jwt.sign( payload, process.env.SECRET_KEY )
+}
+
+
+module.exports = {
+    createToken
+}
